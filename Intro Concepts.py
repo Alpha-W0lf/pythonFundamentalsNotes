6a021d9f4bf7b58868671a9162a0289ef99e7a99
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-17 09:52:13 -0600

    Update Intro Concepts.py

---
+++ Intro Concepts.py
@@ -0,0 +1,9 @@
+# can always use help(object) to see how an object works.
+# help(float)
+# Data types:
+# data structures are dictionaries, tuples, lists
+# sets can be found in the sets library (available from python 2.5 onwards)
+# lists are similar to one-dimensional arrays, although you can have lists nested inside lists
+# dictionaries are essentially "associative arrays" or hash tables
+# tuples are one-dimensional arrays
+# String: plain text data format. can concatinate, but cannot do arithmatic.
